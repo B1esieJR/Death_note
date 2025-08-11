5f7d06b44cb744a95cb329bf92ad595a48ea1f0a
B1esie <b1esiework@gmail.cpm>
2025-08-11 17:19:13 +0700

    Состояния 2 коммит

--- Death_note/src/App.jsx
+++ Death_note/src/App.jsx
@@ -2,6 +2,11 @@ import './App.css';
 import Button from './components/Button/Button';
 import JornalItem from './components/JornalItem/JornalItem';
 import CardButton from "./components/CardButton/CardButton";
+import LeftPanel from "./layouts/leftPanel/LeftPanel.jsx";
+import Body from "./layouts/Body/Body.jsx";
+import Header from "./components/Header/Header.jsx";
+import JournalList from "./components/JournalList/JournalList.jsx";
+import JournalAddButton from "./components/JournalAddButton/JournalAddButton.jsx";
 
 function App() {
     const data = [
@@ -16,30 +21,43 @@ function App() {
             date: new Date(),
         },
 
-    ]
+    ];
+    const InputChange=(event)=>{
+        console.log(event.target.value);
+    };
     return (
-        <>
-            <h1>123</h1>
-            <p>123</p>
+        <div className="App">
+          <LeftPanel>
+          <Header/>
+          <JournalAddButton/>
+          <JournalList>
+              <CardButton>
+                  <JornalItem
+                      title={data[0].title}
+                      text={data[0].text}
+                      date={data[0].date}
+                  />
+              </CardButton>
+              <CardButton>
+                  <JornalItem
+                      title={data[1].title}
+                      text={data[1].text}
+                      date={data[1].date}
+                  />
+              </CardButton>
+          </JournalList>
+          </LeftPanel>
+        <Body>
+            <input type={"text"} onChange={InputChange}/>
             <Button/>
-            <CardButton>
-                Новое воспоминание
-            </CardButton>
-            <CardButton>
-            <JornalItem
-                title={data[0].title}
-                text={data[0].text}
-                date={data[0].date}
-            />
-            </CardButton>
-            <CardButton>
-            <JornalItem
-                title={data[1].title}
-                text={data[1].text}
-                date={data[1].date}
-            />
-            </CardButton>
-        </>
+        </Body>
+
+
+
+
+
+
+        </div>
     )
 }
 
